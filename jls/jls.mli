870ccbf9cbafb2f7f55aa3af39f627be41045a21
Julius Alexander IV <julius.alexander@sofarocean.com>
2024-12-01 19:45:33 -0800

    Move jls.ml into lib

--- jls/jls.mli
+++
@@ -1,3 +0,0 @@
-val unzip : ('a * 'b) list -> 'a list * 'b list
-
-val zip : 'a list -> 'b list -> ('a * 'b) list
---
+++ lib/jls.mli
@@ -0,0 +1,5 @@
+(* This is like a C header file and defines the public interface of the functions, otherwise everything in jls.ml is visible *)
+
+val unzip : ('a * 'a) list -> 'a list * 'a list
+
+val zip : 'a list -> 'a list -> ('a * 'a) list
